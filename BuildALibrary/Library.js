6858ae432bd6214806d295c0ecf01b6100b75789
David Klein <davekl1999@gmail.com>
2021-09-28 01:34:37 -0400

    add media class

---
+++ BuildALibrary/Library.js
@@ -0,0 +1,13 @@
+class Media {
+    constructor(title) {
+        this._title = title;
+        this._isCheckedOut = false;
+        this._ratings = [];
+    }
+    get title() {
+        return this._title;
+    }
+    get isCheckedout() {
+        return this._isCheckedOut
+    }
+}
