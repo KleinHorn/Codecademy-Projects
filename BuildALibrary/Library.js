97ecf49f56e2fd786eeb671340b609b497ad3fa5
David Klein <davekl1999@gmail.com>
2021-09-28 02:01:28 -0400

    add working addRating method

--- BuildALibrary/Library.js
+++ BuildALibrary/Library.js
@@ -19,13 +19,15 @@ class Media {
     toggleCheckOutStatus() {
         this._isCheckedOut = !this._isCheckedOut;
     }
+    addRating(rating) {
+        this._ratings.push(rating);
+    }
 }
 
-
-const blah = new Media('Blah');
+const blah = new Media("Blah");
 blah._ratings = [9, 9, 9, 8];
 console.log(blah.ratings);
 console.log(blah.getAverageRating());
-console.log(blah.isCheckedout) // should return false
+console.log(blah.isCheckedout); // should return false
 blah.toggleCheckOutStatus();
-console.log(blah.isCheckedout) // should return true 
+console.log(blah.isCheckedout); // should return true
