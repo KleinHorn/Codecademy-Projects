196cf83ec9d715f3108392523cea9fa1526a6ee8
David Klein <davekl1999@gmail.com>
2021-09-28 20:49:25 -0400

    add getter and setter methods for School

---
+++ School-Catalogue/Catalogue.js
@@ -0,0 +1,19 @@
+class School {
+    constructor(name, level, numberOfStudents) {
+        this.name = name;
+        this.level = level;
+        this.numberOfStudents = numberOfStudents;
+    }
+    get name() {
+        return this.name;
+    }
+    get level() {
+        return this.level;
+    }
+    get numberOfStudents() {
+        return this.numberOfStudents;
+    }
+    set numberOfStudents(n) {
+        this.numberOfStudents = n;
+    }
+}
