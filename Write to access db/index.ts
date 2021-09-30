51de32c5be918b7083561ddac5b97f7641b5cd95
David Klein <davekl1999@gmail.com>
2021-09-30 11:04:34 -0400

    write to microsoft access

---
+++ Write to access db/index.ts
@@ -0,0 +1,13 @@
+// Get the adodb module
+var ADODB = require('node-adodb');
+ADODB.debug = true;
+
+// Connect to the MS Access DB
+var connection = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\dbs\\my-access-db.accdb;Persist Security Info=False;');
+
+// Query the DB
+connection
+    .query('SELECT * FROM [TestTable];')
+    .on('done', function (data){
+        console.log('Result:'.green.bold, data);
+    })
